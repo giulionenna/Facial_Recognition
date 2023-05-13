e7f92a5adfb047e329466fbfd80f6a3df589c920
giulio524 <62382801+giulionenna@users.noreply.github.com>
2023-05-13 02:01:45 +0200

    andato avanti su test phase, ora funziona prima non funzionava veramente. Da inventarsi alcuni plot da mettere sul latex

--- facial_recognition.py
+++ facial_recognition.py
@@ -9,8 +9,9 @@ path = 'archive/archive'
 num_subjects = 40
 num_faces_per_subject = 10
 train_test_ratio = 0.6
-variance_threshold = 0.8
-acceptance_threshold =  4*10**3
+variance_threshold_vec = [0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.95]
+acceptance_threshold =  6*10**3
+plot_flag = False
 
 train_set_size = int(num_subjects*num_faces_per_subject*train_test_ratio)
 test_set_size = int(num_subjects*num_faces_per_subject*(1-train_test_ratio))
@@ -35,7 +36,7 @@ for i in range(num_subjects): #for each subject
     
     faces_all[i*num_faces_per_subject:(i+1)*num_faces_per_subject,:] = subject_faces
     faces_train[i*int(num_faces_per_subject*train_test_ratio):(i+1)*int(num_faces_per_subject*train_test_ratio),:] = subject_faces[0:int(num_faces_per_subject*train_test_ratio),:]
-    faces_test[i*int(num_faces_per_subject*(1-train_test_ratio)):(i+1)*int(num_faces_per_subject*(1-train_test_ratio)),:] = subject_faces[0:int(num_faces_per_subject*(1-train_test_ratio)),:]
+    faces_test[i*int(num_faces_per_subject*(1-train_test_ratio)):(i+1)*int(num_faces_per_subject*(1-train_test_ratio)),:] = subject_faces[int(num_faces_per_subject*train_test_ratio):num_faces_per_subject,:]
 
 # -------- Training phase ---------------
 L =  faces_train.shape[0]
@@ -49,30 +50,54 @@ eig_val = eig_val[sort_mask]
 eig_vec = eig_vec[:, sort_mask]
 
 explained_variance_cum = (eig_val/eig_val.sum()).cumsum() #computing the cumulative explained variance
-threshold_mask = explained_variance_cum<variance_threshold
-num_pcs_kept = (threshold_mask).sum()
-print('Using variance threshold: '+str(variance_threshold)+'. Number of PCs kept: '+str(num_pcs_kept)+'/'+str(train_set_size))
-eigenfaces = faces_train_center.transpose() @ eig_vec[:,threshold_mask] #computing eigenvalues of the original covariance matrix
-eigenfaces = normalize(eigenfaces, axis=0, norm='l2') #normalize by columns the eigenvectors (eigenfaces is now an orthonormal matrix)
-faces_train_projected = faces_train_center @ eigenfaces
 
+count = 0
+accuracy = np.zeros(len(variance_threshold_vec))
+for variance_threshold in variance_threshold_vec:
+    threshold_mask = explained_variance_cum<variance_threshold
+    num_pcs_kept = (threshold_mask).sum()
+    print('Using variance threshold: '+str(variance_threshold)+'\t Number of PCs kept: '+str(num_pcs_kept)+'/'+str(train_set_size))
+    eigenfaces = faces_train_center.transpose() @ eig_vec[:,threshold_mask] #computing eigenvalues of the original covariance matrix
+    eigenfaces = normalize(eigenfaces, axis=0, norm='l2') #normalize by columns the eigenvectors (eigenfaces is now an orthonormal matrix)
+    faces_train_projected = faces_train_center @ eigenfaces
 
-#--------- TEST PHASE -------------
-faces_test_centered = faces_test-mean_face
-faces_test_projected = faces_test_centered @ eigenfaces #project test faces onto eigenspace
-faces_test_projected_back = faces_test_projected @ eigenfaces.transpose() #project back onto face space
-distance_from_face_space = np.linalg.norm(faces_test_centered-faces_test_projected_back, axis=1) #compute distance from eigenspace for each face
+    #--------- TEST PHASE -------------
+    faces_test_centered = faces_test-mean_face
+    faces_test_projected = faces_test_centered @ eigenfaces #project test faces onto eigenspace
+    faces_test_projected_back = faces_test_projected @ eigenfaces.transpose() #project back onto face space
+    distance_from_face_space = np.linalg.norm(faces_test_centered-faces_test_projected_back, axis=1) #compute distance from eigenspace for each face
 
-predicted = -1*np.ones(test_set_size)
+    predicted = -1*np.ones(test_set_size) #init prediction vector
+    true_faces = np.zeros(test_set_size) #init true classification idx
+    test_faces_per_subject = int(num_faces_per_subject*(1-train_test_ratio))
+    for i in range(num_subjects):
+        true_faces[i*test_faces_per_subject:(i+1)*test_faces_per_subject] = i
 
-for i in range(test_set_size):
-   face = faces_test_projected[i]
-   if distance_from_face_space[i]>acceptance_threshold:
-       continue
-   dist = np.linalg.norm(faces_train_projected - face, axis=1)
-   idx = np.argmin(dist)
-   predicted[i] = idx // int(num_faces_per_subject*train_test_ratio)
+    if(plot_flag and num_pcs_kept>1):
+        train_idx = np.zeros(train_set_size)
+        train_faces_per_subject = int(num_faces_per_subject*(train_test_ratio))
+        for i in range(num_subjects):
+            train_idx[i*train_faces_per_subject:(i+1)*train_faces_per_subject] = i
 
-   
-       
-print('Test phase')
+        plt.scatter(faces_train_projected[:,0], faces_train_projected[:,1], c=train_idx)
+        plt.scatter(faces_test_projected[:,0], faces_test_projected[:,1], c=true_faces, marker='x')
+        plt.show()
+
+    #Prediction
+    for i in range(test_set_size):#for each face in the test set
+        face = faces_test_projected[i]
+        if distance_from_face_space[i]>acceptance_threshold: #if the face is too far from eigenspace
+            continue #skip face
+        dist = np.linalg.norm(faces_train_projected - face, axis=1) #compute distance of the face with every other face in the eigenspace
+        idx = np.argmin(dist) #find the nearest face
+        predicted[i] = idx // int(num_faces_per_subject*train_test_ratio) #predict using idx of the nearest face
+
+    accuracy[count] = sum(true_faces == predicted)/test_set_size
+    unrecognized = sum(predicted == -1)
+    print('Accuracy obtained: '+str(accuracy[count])+'\t Recognized faces: '+str(test_set_size-unrecognized)+'/'+str(test_set_size))
+    count += 1
+
+plt.plot(variance_threshold_vec, accuracy)
+plt.show()
+        
+    
